84f9521ebdffec2a2dd604e1f698f18ba070778e
Michael P. Gerlek <mgerlek@inferlink.com>
2024-01-17 17:54:11 +0000

    brought over rest of docker and tasking files

---
+++ integration/docker-tools/build.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+set -e
+
+do_pull=0
+do_build=0
+do_push=0
+
+while (($#))
+do
+    case $1 in
+	--pull) do_pull=1; shift;;
+	--build) do_build=1; shift;;
+	--push) do_push=1; shift;;
+	*) echo "usage: build.sh [--pull] [--build] [--push]" ; false 
+    esac
+done
+
+
+. ./modules.sh
+
+if [ "$do_pull" -eq 1 ]
+then
+    for i in $MODULE_IMAGES
+    do
+	docker pull $i
+    done
+fi
+
+if [ "$do_build" -eq 1 ]
+then
+    for i in $MODULE_DIRS
+    do
+	pushd $REPO_ROOT/$i/docker
+	./build_docker.sh
+	popd
+    done
+fi
+
+if [ "$do_push" -eq 1 ]
+then
+    for i in $MODULE_IMAGES
+    do
+	docker push $i
+    done
+fi
