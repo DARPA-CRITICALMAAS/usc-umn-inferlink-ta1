832ca6962489c04b7234bdd7cc73c5ae2cf5ce75
Michael P. Gerlek <mgerlek@inferlink.com>
2024-01-17 17:38:01 +0000

    added the rest of the docker dirs

--- polygon/docker/build_docker.sh
+++ polygon/docker/build_docker.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
 set -e
 
-docker pull pytorch/pytorch
-
 $REPO_ROOT/integration/docker-tools/dockerfile-include.py \
     Dockerfile Dockerfile.tmp $REPO_ROOT/integration/docker-tools
 
 docker build -t inferlink/ta1_polygon_extract -f Dockerfile.tmp $REPO_ROOT
-
-docker push inferlink/ta1_polygon_extract
 
