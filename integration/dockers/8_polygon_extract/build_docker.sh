52149c2e03fb7569b3c0d7e0f56ed0e209c61bea
Michael P. Gerlek <mpg@flaxen.com>
2024-02-05 11:03:33 -0800

    deleted integration code -- now lives in separate repo (#6)
    * docs from Sat.
    
    * draft done
    
    * moved to new repo
    
    ---------
    
    Co-authored-by: Michael P. Gerlek <mgerlek@inferlink.com>

--- integration/dockers/8_polygon_extract/build_docker.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-
-$REPO_ROOT/integration/docker-tools/dockerfile-include.py \
-    Dockerfile Dockerfile.tmp $REPO_ROOT/integration/docker-tools
-
-docker build -t inferlink/ta1_polygon_extract -f Dockerfile.tmp $REPO_ROOT
-
