fcdb74e81fb47a4182b6328c68b0c7dfe94bca8e
Weiwei Duan <weiweidu@usc.edu>
2024-03-07 11:30:03 -0800

    fix the bug where the input images are too large for GPT4

--- segmentation/legend_item_description_segment/src/gpt4_input_generation.py
+++ segmentation/legend_item_description_segment/src/gpt4_input_generation.py
@@ -6,12 +6,38 @@ the cropped images are inputs for GPT4
 import os
 import cv2
 import json
-import numpy as np    
+import numpy as np
+from PIL import Image
 import time
+import collections
+import io
+
+def get_image_size(image):
+    img_pil = Image.fromarray(image)
+    # Save the image to an in-memory bytes buffer
+    img_buffer = io.BytesIO()
+    img_pil.save(img_buffer, format='PNG')
+    # Calculate the size in MB
+    img_size_mb = img_buffer.getbuffer().nbytes / (1024 * 1024)
+    return img_size_mb
     
+def crop_image_further(bbox_list, s_col, width, height):
+    gpt_input_image_bboxes = []
+    bbox_list = list(bbox_list)
+    bbox_list.sort(key=lambda x:x[1]) # sorted by row
+    num_bbox = len(bbox_list)
+    # divide the height into three parts
+    row_ranges = np.linspace(0, num_bbox-1, num=4).astype('int32')
+    for i in range(1, row_ranges.shape[0]):
+        s_row, e_row = bbox_list[row_ranges[i-1]][1], bbox_list[row_ranges[i]][1]
+        e_col = s_col + width
+        gpt_input_image_bboxes.append([s_col, e_col, s_row, e_row])
+    return gpt_input_image_bboxes
+    
+
 def generate_gpt4_input(column_bboxes, roi, map_tif, map_name, save_gpt_input_dir):
     x, y, w, h = np.array(roi).astype('int32')
-    columns_png = {}
+    columns_png = collections.defaultdict(list)
     
     if not os.path.exists(save_gpt_input_dir):
         os.mkdir(save_gpt_input_dir)
@@ -21,7 +47,7 @@ def generate_gpt4_input(column_bboxes, roi, map_tif, map_name, save_gpt_input_di
         gpt_input_png_path = os.path.join(save_gpt_input_dir, img_name)
         gpt_input_png = map_tif[y:y+h, x:x+w]
         cv2.imwrite(gpt_input_png_path, gpt_input_png)
-        columns_png['0.0'] = gpt_input_png_path
+        columns_png['0.0'] = [img_name]
         return columns_png
     
     sorted_columns = sorted(column_bboxes.keys())
@@ -43,13 +69,22 @@ def generate_gpt4_input(column_bboxes, roi, map_tif, map_name, save_gpt_input_di
 
         if abs(s_row-e_row) < 10 or abs(s_col-e_col) < 10:
             continue
-
-        # columns_png save the gpt input png name for one column
-        columns_png[col] = f'{map_name}_{int(s_col)}_{int(s_row)}_{int(e_col)-int(s_col)}_{int(e_row)-int(s_row)}.png'
-            
-        gpt_input_png_path = os.path.join(save_gpt_input_dir, columns_png[col])
+        
         gpt_input_png = map_tif[s_row:e_row, s_col:e_col]
-        cv2.imwrite(gpt_input_png_path, gpt_input_png)
+        
+        if get_image_size(gpt_input_png) > 18: #image size > 18MB
+            gpt_input_bbox = crop_image_further(bbox_np, s_col, int(e_col)-int(s_col), int(e_row)-int(s_row))
+        else:
+            gpt_input_bbox = [[s_col, e_col, s_row, e_row]]
+        
+        # columns_png save the gpt input png name for one column
+        for s_col, e_col, s_row, e_row in gpt_input_bbox:
+            png_name = f'{map_name}_{int(s_col)}_{int(s_row)}_{int(e_col)-int(s_col)}_{int(e_row)-int(s_row)}.png'
+            columns_png[col].append(png_name)
+            
+            gpt_input_png_path = os.path.join(save_gpt_input_dir, png_name)
+            gpt_input_png = map_tif[s_row:e_row, s_col:e_col]
+            cv2.imwrite(gpt_input_png_path, gpt_input_png)
     return columns_png
         
     
