643ae67a23c94aecb22570455d57f733e86655f5
Ubuntu <ubuntu@ip-10-0-0-225.us-west-2.compute.internal>
2024-07-01 07:39:25 +0000

    remove unused functions

--- system/mapkurator/spotter-v2/tools/predictor.py
+++ system/mapkurator/spotter-v2/tools/predictor.py
@@ -6,11 +6,11 @@ import multiprocessing as mp
 from collections import deque
 import cv2
 import torch
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
 from detectron2.data import MetadataCatalog
 from detectron2.engine.defaults import DefaultPredictor
-from detectron2.utils.video_visualizer import VideoVisualizer
+# from detectron2.utils.video_visualizer import VideoVisualizer
 from detectron2.utils.visualizer import ColorMode, Visualizer
 
 from adet.utils.visualizer import TextVisualizer
@@ -109,77 +109,77 @@ class VisualizationDemo(object):
             else:
                 break
 
-    def vis_bases(self, bases):
-        basis_colors = [[2, 200, 255], [107, 220, 255], [30, 200, 255], [60, 220, 255]]
-        bases = bases[0].squeeze()
-        bases = (bases / 8).tanh().cpu().numpy()
-        num_bases = len(bases)
-        fig, axes = plt.subplots(nrows=num_bases // 2, ncols=2)
-        for i, basis in enumerate(bases):
-            basis = (basis + 1) / 2
-            basis = basis / basis.max()
-            basis_viz = np.zeros((basis.shape[0], basis.shape[1], 3), dtype=np.uint8)
-            basis_viz[:, :, 0] = basis_colors[i][0]
-            basis_viz[:, :, 1] = basis_colors[i][1]
-            basis_viz[:, :, 2] = np.uint8(basis * 255)
-            basis_viz = cv2.cvtColor(basis_viz, cv2.COLOR_HSV2RGB)
-            axes[i // 2][i % 2].imshow(basis_viz)
-        plt.show()
+    # def vis_bases(self, bases):
+    #     basis_colors = [[2, 200, 255], [107, 220, 255], [30, 200, 255], [60, 220, 255]]
+    #     bases = bases[0].squeeze()
+    #     bases = (bases / 8).tanh().cpu().numpy()
+    #     num_bases = len(bases)
+    #     fig, axes = plt.subplots(nrows=num_bases // 2, ncols=2)
+    #     for i, basis in enumerate(bases):
+    #         basis = (basis + 1) / 2
+    #         basis = basis / basis.max()
+    #         basis_viz = np.zeros((basis.shape[0], basis.shape[1], 3), dtype=np.uint8)
+    #         basis_viz[:, :, 0] = basis_colors[i][0]
+    #         basis_viz[:, :, 1] = basis_colors[i][1]
+    #         basis_viz[:, :, 2] = np.uint8(basis * 255)
+    #         basis_viz = cv2.cvtColor(basis_viz, cv2.COLOR_HSV2RGB)
+    #         axes[i // 2][i % 2].imshow(basis_viz)
+    #     plt.show()
 
-    def run_on_video(self, video):
-        """
-        Visualizes predictions on frames of the input video.
+    # def run_on_video(self, video):
+    #     """
+    #     Visualizes predictions on frames of the input video.
 
-        Args:
-            video (cv2.VideoCapture): a :class:`VideoCapture` object, whose source can be
-                either a webcam or a video file.
+    #     Args:
+    #         video (cv2.VideoCapture): a :class:`VideoCapture` object, whose source can be
+    #             either a webcam or a video file.
 
-        Yields:
-            ndarray: BGR visualizations of each video frame.
-        """
-        video_visualizer = VideoVisualizer(self.metadata, self.instance_mode)
+    #     Yields:
+    #         ndarray: BGR visualizations of each video frame.
+    #     """
+    #     video_visualizer = VideoVisualizer(self.metadata, self.instance_mode)
 
-        def process_predictions(frame, predictions):
-            frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-            if "panoptic_seg" in predictions:
-                panoptic_seg, segments_info = predictions["panoptic_seg"]
-                vis_frame = video_visualizer.draw_panoptic_seg_predictions(
-                    frame, panoptic_seg.to(self.cpu_device), segments_info
-                )
-            elif "instances" in predictions:
-                predictions = predictions["instances"].to(self.cpu_device)
-                vis_frame = video_visualizer.draw_instance_predictions(frame, predictions)
-            elif "sem_seg" in predictions:
-                vis_frame = video_visualizer.draw_sem_seg(
-                    frame, predictions["sem_seg"].argmax(dim=0).to(self.cpu_device)
-                )
+    #     def process_predictions(frame, predictions):
+    #         frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+    #         if "panoptic_seg" in predictions:
+    #             panoptic_seg, segments_info = predictions["panoptic_seg"]
+    #             vis_frame = video_visualizer.draw_panoptic_seg_predictions(
+    #                 frame, panoptic_seg.to(self.cpu_device), segments_info
+    #             )
+    #         elif "instances" in predictions:
+    #             predictions = predictions["instances"].to(self.cpu_device)
+    #             vis_frame = video_visualizer.draw_instance_predictions(frame, predictions)
+    #         elif "sem_seg" in predictions:
+    #             vis_frame = video_visualizer.draw_sem_seg(
+    #                 frame, predictions["sem_seg"].argmax(dim=0).to(self.cpu_device)
+    #             )
 
-            # Converts Matplotlib RGB format to OpenCV BGR format
-            vis_frame = cv2.cvtColor(vis_frame.get_image(), cv2.COLOR_RGB2BGR)
-            return vis_frame
+    #         # Converts Matplotlib RGB format to OpenCV BGR format
+    #         vis_frame = cv2.cvtColor(vis_frame.get_image(), cv2.COLOR_RGB2BGR)
+    #         return vis_frame
 
-        frame_gen = self._frame_from_video(video)
-        if self.parallel:
-            buffer_size = self.predictor.default_buffer_size
+    #     frame_gen = self._frame_from_video(video)
+    #     if self.parallel:
+    #         buffer_size = self.predictor.default_buffer_size
 
-            frame_data = deque()
+    #         frame_data = deque()
 
-            for cnt, frame in enumerate(frame_gen):
-                frame_data.append(frame)
-                self.predictor.put(frame)
+    #         for cnt, frame in enumerate(frame_gen):
+    #             frame_data.append(frame)
+    #             self.predictor.put(frame)
 
-                if cnt >= buffer_size:
-                    frame = frame_data.popleft()
-                    predictions = self.predictor.get()
-                    yield process_predictions(frame, predictions)
+    #             if cnt >= buffer_size:
+    #                 frame = frame_data.popleft()
+    #                 predictions = self.predictor.get()
+    #                 yield process_predictions(frame, predictions)
 
-            while len(frame_data):
-                frame = frame_data.popleft()
-                predictions = self.predictor.get()
-                yield process_predictions(frame, predictions)
-        else:
-            for frame in frame_gen:
-                yield process_predictions(frame, self.predictor(frame))
+    #         while len(frame_data):
+    #             frame = frame_data.popleft()
+    #             predictions = self.predictor.get()
+    #             yield process_predictions(frame, predictions)
+    #     else:
+    #         for frame in frame_gen:
+    #             yield process_predictions(frame, self.predictor(frame))
 
 
 class AsyncPredictor:
